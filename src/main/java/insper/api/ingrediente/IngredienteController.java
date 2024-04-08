2eecd373ab7663658bc93d65917a81e78cf2e251
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2024-04-08 14:30:38 -0300

    a

--- src/main/java/insper/api/ingrediente/IngredienteController.java
+++ src/main/java/insper/api/ingrediente/IngredienteController.java
@@ -2,6 +2,7 @@ package insper.api.ingrediente;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,4 +32,8 @@ public interface IngredienteController {
     @GetMapping("/ingredientes")
     public ResponseEntity<IngredienteOut> readAll();
     
+    @DeleteMapping("/ingredientes/{id}")
+    public ResponseEntity<IngredienteOut> delete(
+        @PathVariable(required = true) String id
+    );
 }
